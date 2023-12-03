4075a7723df4a6d8e8f543f7b4f08dd59d96374d
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-12-03 12:47:16 -0300

    feat: criando header, footer e poke-filter

---
+++ src/app/components/poke-filter/poke-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './poke-filter.component.html',
+  styleUrl: './poke-filter.component.scss'
+})
+export class PokeFilterComponent {
+
+}
---
+++ src/app/mock/pokeGenerations/index.ts
@@ -0,0 +1,47 @@
+const pokeGenerations = [
+    {
+        name:'1° Geração',
+        limit:151,
+        offset:0
+    },
+    {
+        name:'2° Geração',
+        limit:151,
+        offset:0
+    },
+    {
+        name:'3° Geração',
+        limit:151,
+        offset:0
+    },
+    {
+        name:'4° Geração',
+        limit:151,
+        offset:0
+    },
+    {
+        name:'5° Geração',
+        limit:151,
+        offset:0
+    },
+    {
+        name:'6° Geração',
+        limit:151,
+        offset:0
+    },
+    {
+        name:'7° Geração',
+        limit:151,
+        offset:0
+    },
+    {
+        name:'8° Geração',
+        limit:151,
+        offset:0
+    },
+    {
+        name:'9° Geração',
+        limit:151,
+        offset:0
+    },
+]
